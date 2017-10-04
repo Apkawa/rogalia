a8985101a63b2f51db7b0de401eaa5643fd9d62f
dKt <rogalia.gss@gmail.com>
2017-10-05 00:33:03 +0900

    Update talks.js (#205)

--- src/lang/ja/talks.js
+++ src/lang/ja/talks.js
@@ -218,7 +218,7 @@ Talks.npcs = {
         },
         "actions": {
             "Trade": "商品を見せてください。",
-            "Repair": "I want to repair my stuff",
+            "Repair": "修理してほしい。",
         },
     },
     "boris": {
